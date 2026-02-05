3a07a88346ba4cc43ab956d4dc2c37b92c08aad3
simon7968 <simonjohn7968@gmail.com>
2026-02-05 15:33:28 +0530

    new

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
-
-@app.route("/health")
-def health():
-    return {"status": "UP"}
+    return "AutoDeployX is running 🚀"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
