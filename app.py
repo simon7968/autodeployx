200b71a64a08dd74a4b2e9ccec95860d3d046184
simon7968 <simonjohn7968@gmail.com>
2026-02-08 14:20:03 +0530

    Test auteplot

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "AutoDeployX updated successfully 🚀"
+    return render_template("index.html")  # ✅ HTML loads
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run()
