12236a2f66289205fb2e1889f0f5d13c9ed53a08
simon7968 <simonjohn7968@gmail.com>
2026-02-11 14:00:46 +0530

    Test auteeeplot

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "AutoDeployX is running 🚀"
+    return "AutoDeployX is running succesfully  🚀"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
